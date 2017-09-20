d66a3033f9c14c0176954280d9772018769a7e6b
Laurent Nocca <lnocca@fanreact.com>
2017-09-19 20:39:57 -0500

    finished services practice

---
+++ src/app/new-account/new-account.component.ts
@@ -0,0 +1,24 @@
+import { Component} from '@angular/core';
+import { LoggingService} from '../logging.service'
+import {AccountsService} from '../accounts.service';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css'],
+  // providers: [LoggingService]
+})
+export class NewAccountComponent {
+
+  constructor(private loggingService: LoggingService,
+              private accountsService: AccountsService,){
+    this.accountsService.statusUpdated.subscribe(
+      (status: string) => console.log('New Status ' + status)
+    );
+  } // by defining the service in the constructor, Angular gives us an instance of it
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountsService.addAccount(accountName, accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
+  }
+}
